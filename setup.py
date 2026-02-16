a12d401c6aabd3f713dc8fb5b664bf7ab8597ccd
d60 <kaweco2007@outlook.jp>
2026-02-17 02:49:21 +0900

    te

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup
 
 setup(
     name='twitter_castle_token',
-    version='0.0.1',
+    version='0.0.2',
     install_requires=[
+        'mmh'
     ],
     python_requires='>=3.10',
 )
