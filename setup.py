5c2f09964357aade499652b9f3cba2ba377f1804
d60 <kaweco2007@outlook.jp>
2026-02-17 03:00:01 +0900

    test

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='twitter_castle_token',
-    version='0.0.2',
+    version='0.0.1',
     install_requires=[
         'mmh3'
     ],
     python_requires='>=3.10',
-    packages=find_packages(include=['castle'])
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
 )
